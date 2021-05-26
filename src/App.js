4879abf77d78cc64de77f9d3ed3f99056317459c
Jovan Bienvenu <jbienvenu@wisersoftware.com>
2021-05-26 16:23:07 +0200

    Load and list restaurants

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import {Provider} from 'react-redux';
+import store from './store';
 import RestaurantScreen from './components/RestaurantScreen';
 
 const App = () => {
   return (
-    <div>
+    <Provider store={store}>
       <RestaurantScreen />
-    </div>
+    </Provider>
   );
 };
 
--- src/components/__tests__/RestaurantList.spec.js
+++ src/components/__tests__/RestaurantList.spec.js
@@ -2,11 +2,33 @@ import {render} from '@testing-library/react';
 import {RestaurantList} from '../RestaurantList';
 
 describe('RestaurantList', () => {
+  const restaurants = [
+    {id: 1, name: 'Sushi Place'},
+    {id: 2, name: 'Pizza Place'},
+  ];
+
+  let loadRestaurants;
+  let context;
+
+  beforeEach(() => {
+    loadRestaurants = jest.fn().mockName('loadRestaurants');
+
+    context = render(
+      <RestaurantList
+        loadRestaurants={loadRestaurants}
+        restaurants={restaurants}
+      />,
+    );
+  });
+
   it('Loads restaurants on first render', () => {
-    const loadRestaurants = jest.fn().mockName('loadRestaurants');
-
-    render(<RestaurantList loadRestaurants={loadRestaurants} />);
-
     expect(loadRestaurants).toHaveBeenCalled();
+  });
+
+  it('Displays the restaurants', () => {
+    const {queryByText} = context;
+
+    expect(queryByText('Sushi Place')).not.toBeNull();
+    expect(queryByText('Pizza Place')).not.toBeNull();
   });
 });
