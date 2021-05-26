66f1b47ebaff333d7f404d12340cd7fccbbf8e6f
Jovan Bienvenu <jbienvenu@wisersoftware.com>
2021-05-26 16:43:47 +0200

    Style app with Material Design

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import {Provider} from 'react-redux';
+import {createMuiTheme} from '@material-ui/core/styles';
+import blueGrey from '@material-ui/core/colors/blueGrey';
+import {ThemeProvider} from '@material-ui/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
 import store from './store';
 import RestaurantScreen from './components/RestaurantScreen';
 
-const App = () => {
-  return (
-    <Provider store={store}>
-      <RestaurantScreen />
-    </Provider>
-  );
-};
+const theme = createMuiTheme({
+  palette: {
+    primary: blueGrey,
+  },
+});
+
+const App = () => (
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">Opinion Ate</Typography>
+        </Toolbar>
+      </AppBar>
+      <Container>
+        <RestaurantScreen />
+      </Container>
+    </ThemeProvider>
+  </Provider>
+);
 
 export default App;
